66955b48d275810cd99136b00b5aea7175f3f228
EFH5 <francia.ej@obmontessori.edu.ph>
2025-09-21 19:15:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from pyscript import document
+
+def gather_info(event): 
+    
+    name = str(document.getElementById("name"))
+    age = str(document.getElementById("age"))
+    school = str(document.getElementById("school"))
+
+info = "{name} is currently {age} years old and studies at {school} \n This information was gathered through input fields and displayed using \t a multiline string in Python via Pyscript"
