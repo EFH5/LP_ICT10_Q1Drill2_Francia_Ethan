e967e767b614361f95445940a2a95b985cbd156c
EFH5 <francia.ej@obmontessori.edu.ph>
2025-09-21 19:24:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from pyscript import document
 
 def gather_info(event): 
-    
-    name = str(document.getElementById("name"))
-    age = str(document.getElementById("age"))
-    school = str(document.getElementById("school"))
+
+    name = document.getElementById("name").value
+    age = document.getElementById("age").value
+    school = document.getElementById("school").value
+
+    document.getElementById("name").innerText = name
+    document.getElementById("age").innerText = age
+    document.getElementById("school").innerText = school
 
 info = "{name} is currently {age} years old and studies at {school} \n This information was gathered through input fields and displayed using \t a multiline string in Python via Pyscript"
 document.getElementById("info").innerText = info
-
